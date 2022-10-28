25892f3586e0087e9055119e3bbb989d23c13296
unknown <jakub.czechowski@interia.pl>
2022-10-28 12:49:08 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/DolFragment.java
@@ -0,0 +1,61 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.location.GnssAntennaInfo;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class DolFragment extends Fragment {
+
+    Button button;
+    EditText editText;
+    DolFragment.FragmentDListener listener;
+
+    public interface  FragmentDListener{
+        void onInputDSent (CharSequence input);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_dol, container, false);
+        button = view.findViewById(R.id.button6);
+        editText = view.findViewById(R.id.editText2);
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        CharSequence input = editText.getText();
+                        listener.onInputDSent(input);
+                    }
+                }
+        );
+        return view;
+    }
+
+    public void zaktualizujEditText(CharSequence nowyText) {
+        editText.setText(nowyText);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        listener = (FragmentDListener)context;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        listener = null;
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/GornyFragment.java
@@ -0,0 +1,62 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class GornyFragment extends Fragment {
+    Button button;
+    EditText editText;
+    FragmentGListener listener;
+
+    public interface  FragmentGListener{
+        void onInputGSent (CharSequence input);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =inflater
+                .inflate(R.layout.fragment_gorny, container, false);
+        button = view.findViewById(R.id.button5);
+        editText = view.findViewById(R.id.editText1);
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String pobranyTekst = editText.getText().toString();
+                        CharSequence input = editText.getText();
+                        listener.onInputGSent(input);
+                    }
+                }
+        );
+        return view;
+    }
+
+    public void zaktualizujEditText(CharSequence nowyText) {
+        editText.setText(nowyText);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        listener = (FragmentGListener)context;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        listener = null;
+    }
+}
